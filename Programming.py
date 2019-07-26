edaf3a4f5ec45fe0c25a780a09a545f6cadb8b20
unknown <anupixie26@gmail.com>
2019-07-26 11:21:06 -0700

    variables and ifs

---
+++ Programming.py
@@ -0,0 +1,60 @@
+
+#name=input("What is your name?")
+#print("Nice to meet you ,",name)
+#ans=input()
+#social_media=input("What is the   social media that you use ?")
+#print("You know that ",social_media,"is not the current trend rigt??")
+#ans=input()
+#username=input("So,What is your username?")
+#print("#NotAVeryGoodOne")
+#--------------------------
+#x = input("what is x?")
+# y= input("What is y?")
+#temp=""
+#temp=x
+#x=y
+#y=temp
+
+
+#print("I swapped x and y, look:")e
+#print("x is now ",x)
+#print("y is now ",y)
+#------------------------
+#ques=input()
+#print("Hey")
+#ques=input()
+#print("I'm fine thank you")
+#ques=input()
+#print("Like Duh, I'm your AI friend ")
+#musiclist=["Rock","Hiphop","Pop"]
+#languagelist=["Korean","Thai","Japanese","Chinese"]
+#bandlist=["BTS","Blackpink","RedVelvet","Exid","Ikon","TXT"]
+
+#music=input("What is your favourite type of music?")
+#if music ==musiclist:
+#    language=input("What language do you prefer?")
+#    if language==languagelist:
+#        band=input("What band do you like??")
+#        if band==bandlist:
+#            song=input("What is your favourite song pal??You are basically family!!")
+#            if song=="Fire"or song=="Boy with love":
+#                print("Luv it !!But my favourite one is Mike Drop")
+#            else:
+#                print("Oh.Not my type so..")
+#       else:
+#            print("We have very different tastes pal!!")  
+#   else:
+#        print("My favourite languagde is korean so ....I have nothing to talk about to you ")     
+#else:
+#    print("Eww!! Not the adventurous type yeah??")    
+#      
+
+#print("This is a programme to find if a number is even or odd.")      
+#number=int(input("Input the number."))
+#if number%2==0:
+#    print("The number is even.")
+#else:
+#   print("The number is odd")    
+
+
+   
